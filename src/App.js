b4030d5c326c3287b9dbe7a191bd1763bdbbc825
drrundel <drrundel@umich.edu>
2024-08-03 19:55:36 -0500

    adding google analytics tracking

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
 import {BrowserRouter} from "react-router-dom";
+import ReactGA from 'react-ga4';
+
 
 function App() {
+    ReactGA.initialize('8535186584');
+
    return (
       <div>
          <BrowserRouter>
